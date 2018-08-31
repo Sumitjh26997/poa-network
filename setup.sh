a0ff9419903e56b6a08d459ddad3294e946e50be
Sumitjh26997 <sumit.hotchandani@gmail.com>
2018-08-31 22:45:19 +0530

    Config file update

--- setup.sh
+++ setup.sh
@@ -5,11 +5,15 @@ sudo apt-get install build-essential openssl libssl-dev libudev-dev
 bash <(curl https://get.parity.io -L)
 
 # FOLDER FOR CHAIN SPEC AND NODE CONFIG
-mkdir ~/parity
-cd ~/parity
+mkdir -v ~/parity
 
-#Save Chain Spec
-sudo cp chain_spec.json ~/parity/chain_spec.json
+#SAVE CHAIN SPEC
+sudo cp -v chain_spec.json ~/parity/chain_spec.json
 
-#Create config for node
-sudo cp config.toml ~/parity/$1.toml
+#CREATE CONFIG FOR NODE
+sudo cp -v config.toml ~/parity/$1.toml
+echo $1 >> ~/parity/node.pwds
+echo -n "[account]" >> ~/parity/$1.toml
+echo -n 'password = ["node.pwds"]' >> ~/parity/$1.toml
+
+
